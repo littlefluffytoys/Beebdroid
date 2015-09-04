7f5966db7f7e328eca8d9e6ad7a93b60a103e2a2
Reuben Scratton <reuben.scratton@gmail.com>
2015-09-04 14:05:31 +0100

    - Migrated project to Android Studio. - Initial port of ARM assembly code to X86

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,30 +2,21 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-## remove -ffunction-sections and -fomit-frame-pointer from the default compile flags
-#TARGET_thumb_release_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_thumb_release_CFLAGS))
-#TARGET_thumb_release_CFLAGS := $(filter-out -fomit-frame-pointer,$(TARGET_thumb_release_CFLAGS))
-#TARGET_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_CFLAGS))
-
-## include libandprof.a in the build
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := andprof
-#LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libandprof.a
-#include $(PREBUILT_STATIC_LIBRARY)
-
-
-## this is your shared library
-#include $(CLEAR_VARS)
-## compile with profiling
-#LOCAL_CFLAGS := -pg
-#LOCAL_STATIC_LIBRARIES := andprof
-
 LOCAL_CFLAGS := -DANDROID_NDK \
                 -DDISABLE_IMPORTGL
                 
 LOCAL_MODULE    := bbcmicro
-LOCAL_SRC_FILES := \
-    6502asm.S \
+
+ifeq ($(TARGET_ARCH),arm)
+    LOCAL_CFLAGS +=  -march=armv6t2 -O9
+    LOCAL_SRC_FILES := 6502asm_arm.S
+endif
+ifeq ($(TARGET_ARCH),x86)
+    LOCAL_CFLAGS += -m32
+    LOCAL_SRC_FILES := 6502asm_x86.S
+endif
+
+LOCAL_SRC_FILES += \
     6502.c \
     8271.c \
     adc.c \
